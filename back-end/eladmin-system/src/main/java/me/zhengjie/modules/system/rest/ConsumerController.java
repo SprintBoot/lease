acad4603855f5af210f3c739a1492c46ed465ba3
zlx <2780311834@qq.com>
2021-06-21 18:55:25 +0800

    wxapp

--- back-end/eladmin-system/src/main/java/me/zhengjie/modules/system/rest/ConsumerController.java
+++ back-end/eladmin-system/src/main/java/me/zhengjie/modules/system/rest/ConsumerController.java
@@ -1,28 +1,19 @@
-/*
-*  Copyright 2019-2020 Zheng Jie
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
 package me.zhengjie.modules.system.rest;
 
 import com.alibaba.fastjson.JSONObject;
+
 import me.zhengjie.annotation.Log;
+import me.zhengjie.modules.system.domain.Address;
 import me.zhengjie.modules.system.domain.Consumer;
+import me.zhengjie.modules.system.service.AddressService;
 import me.zhengjie.modules.system.service.ConsumerService;
 import me.zhengjie.modules.system.service.UserService;
 import me.zhengjie.modules.system.service.dto.ConsumerDto;
 import me.zhengjie.modules.system.service.dto.ConsumerQueryCriteria;
+import me.zhengjie.modules.system.service.mapstruct.ConsumerMapper;
+import me.zhengjie.modules.system.utils.DecodeUtil;
 import me.zhengjie.modules.system.utils.WxUtil;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
@@ -30,18 +21,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.*;
 import java.io.IOException;
+//import java.net.http.HttpClient;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-* @website https://el-admin.vip
-* @author lease
-* @date 2021-06-21
-**/
+ * @website https://el-admin.vip
+ * @author lease
+ * @date 2021-06-21
+ **/
 @RestController
 @RequiredArgsConstructor
 @Api(tags = "api/system/consumer管理")
@@ -49,7 +47,8 @@ import javax.servlet.http.HttpServletResponse;
 public class ConsumerController {
 
     private final ConsumerService consumerService;
-
+    private final AddressService addressService;
+    private final ConsumerMapper consumerMapper;
     private static final long serialVersionUID = 1L;
     private static Logger logger = LoggerFactory.getLogger(ConsumerController.class);
 
@@ -57,31 +56,58 @@ public class ConsumerController {
     @ApiOperation("用户登录")
     @ResponseBody
     @GetMapping(value="/login")
-    public void login(String code) {
+    public ResponseEntity<Object> login(String code,String rawData,String encrypteData,String iv) throws Exception{
 
         String appid = "wxeb551f75d6ae0406";
-
         String secret = "4aa1b8d1dbf31344e3abab18dde1a90a";
-
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid=" + appid + "&secret=" + secret + "&js_code=" + code + "&grant_type=authorization_code";
-
         String str = WxUtil.httpRequest(url, "GET", null);
-
         JSONObject jsonObject = JSONObject.parseObject(str);
+        Consumer consumer = new Consumer();
+        try {
+            String openid = jsonObject.get("openid").toString();
+            String sessionkey = jsonObject.get("session_key").toString();
+            ConsumerQueryCriteria criteria = new ConsumerQueryCriteria();
+            criteria.setOpenId(openid);
+            List<ConsumerDto> clist = consumerService.queryAll(criteria);
+            if(clist.size() ==0 ){
+                JSONObject rawDataJson = JSONObject.parseObject(rawData);
 
-        String openid = jsonObject.get("openid").toString();
+                String nickName = rawDataJson.getString( "nickName" );
+                String gender  = rawDataJson.getString( "gender" );
+                String city = rawDataJson.getString( "city" );
+                String country = rawDataJson.getString( "country" );
+                String province = rawDataJson.getString( "province" );
 
-        ConsumerQueryCriteria criteria = new ConsumerQueryCriteria();
-        criteria.setOpenId(openid);
-        List<ConsumerDto> clist = consumerService.queryAll(criteria);
+                String phonestr = DecodeUtil.decryptData(encrypteData,sessionkey,iv);
+                JSONObject phoneJson = JSONObject.parseObject(phonestr);
+                String phone=phoneJson.getString("phoneNumber");
 
-//        if(clist.size() ==0 ){
-//
-//        }
+                System.out.println(rawDataJson);
+                System.out.println(phoneJson);
 
+                consumer.setOpenId(openid);
+                consumer.setNickname(nickName);
+                consumer.setGender(gender);
+                consumer.setPhone(phone);
 
+                Timestamp time = new Timestamp(System.currentTimeMillis());
+                consumer.setCreateTime(time);
 
+                consumerService.create(consumer);
 
+                Address address = new Address();
+                address.setAddress(country+" "+province+" "+city);
+                address.setConsumerId(consumer.getConsumerId());
+
+                addressService.create(address);
+            }else{
+                consumer = consumerMapper.toEntity( clist.get(0));
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(consumer,HttpStatus.OK);
     }
 
     @Log("导出数据")
